9b37f65a2f418e14845c09e12e1b4e2f1a79443f
pickupst <gd.okanuslu@gmail.com>
2019-08-17 20:25:59 +0300

    Add bus controller script

---
+++ Assets/Scripts/Controllers/busController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class busController : MonoBehaviour
+{
+
+    public float speed = 5f;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveVertical, 0.0f, -moveHorizontal);
+
+        rb.AddForce(movement * speed);
+        rb.AddTorque(Vector3.up * speed * moveHorizontal, ForceMode.Impulse);
+    }
+}
