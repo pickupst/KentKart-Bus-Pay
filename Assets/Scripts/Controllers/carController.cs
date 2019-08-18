809ce9dca900c8449d404e0e7317a329d4153f0b
pickupst <gd.okanuslu@gmail.com>
2019-08-18 08:04:08 +0300

    Edit Car controller script

---
+++ Assets/Scripts/Controllers/carController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carController : MonoBehaviour
+{
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float steerAngle;
+
+    public WheelCollider frontWheelL, frontWheelR;
+    public WheelCollider rearWheelL, rearWheelR;
+
+    public Transform frontTransformL, frontTransformR;
+    public Transform rearTransformL, rearTransformR;
+
+    public float maxSteerAngle = 30;
+    public float motor = 50;
+
+
+    public void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = -Input.GetAxis("Vertical");
+    }
+
+    private void Steer()
+    {
+        steerAngle = maxSteerAngle * horizontalInput;
+        frontWheelL.steerAngle = steerAngle;
+        frontWheelR.steerAngle = steerAngle;
+    }
+
+    private void Accelerate()
+    {
+        frontWheelL.motorTorque = verticalInput * motor;
+        frontWheelR.motorTorque = verticalInput * motor;
+    }
+
+    private void UpdateWheelPoses()
+    {
+        UpdateWheelPose(frontWheelL, frontTransformL);
+        UpdateWheelPose(frontWheelR, frontTransformR);
+        UpdateWheelPose(rearWheelL, rearTransformL);
+        UpdateWheelPose(rearWheelR, rearTransformR);
+    }
+
+    private void UpdateWheelPose(WheelCollider _collider, Transform _transform)
+    {
+        Vector3 _pos = _transform.position;
+        Quaternion _quat = _transform.rotation;
+
+        _collider.GetWorldPose(out _pos, out _quat);
+
+        _transform.position = _pos;
+        _transform.rotation = _quat;
+    }
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        Steer();
+        Accelerate();
+        UpdateWheelPoses();
+    }
+
+}
